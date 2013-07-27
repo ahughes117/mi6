beedb2c664aaf4283bf16f63671c010c31f30918
Alex Hughes <ahughes@ahughes.org>
2013-07-27 14:01:41 +0100

    Added agent table in order to distribute mi6

---
+++ mysql/agent.sql
@@ -0,0 +1,18 @@
+USE mi6;
+
+DROP TABLE IF EXISTS `ip`
+CREATE  TABLE IF NOT EXISTS `ip` (
+  `ip` VARCHAR(16) NOT NULL ,
+  `agent` VARCHAR(200) NOT NULL ,
+  `domain` VARCHAR(255) NOT NULL ,
+  `ip_mask` VARCHAR(16) NULL ,
+  `Processed` TINYINT NOT NULL DEFAULT 0 ,
+  `Hostname` VARCHAR(255) NULL ,
+  `Latitude` DOUBLE NULL ,
+  `Longitude` DOUBLE NULL ,
+  `Timezone` INT NULL ,
+  `PostCode` VARCHAR(10) NULL ,
+  `DateCreated` TIMESTAMP NULL ,
+  `_dateModified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`ip`, `agent`, `domain`))
+ENGINE = MyISAM;
