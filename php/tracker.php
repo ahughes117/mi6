febb79c9ab3808b55298e2ffbc1f5399015389ed
Alex Hughes <alexhughes117@gmail.com>
2013-08-02 19:59:27 +0100

    Fixed minor error in tracker

--- php/tracker.php
+++ php/tracker.php
@@ -3,8 +3,9 @@
 require_once('src/ip.php');
 
 if (isset($_POST['ip'])) {
+    $my_ip = insert_ip();
+    
     $ip = gethostbyname(urldecode($_POST['ip']));
-
     if (filter_var($_POST['ip'], FILTER_VALIDATE_IP))
         $request_ip = insert_request($_POST['ip']);
     elseif (strcmp($ip, $_POST['ip']) != 0)
