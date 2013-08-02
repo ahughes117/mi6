434084e95120cf7642b4e5c67c2858ea52d6a25c
Alex Hughes <alexhughes117@gmail.com>
2013-08-02 20:01:19 +0100

    Refactored badly written code in tracker

--- php/tracker.php
+++ php/tracker.php
@@ -2,16 +2,15 @@
 
 require_once('src/ip.php');
 
+$my_ip = insert_ip();
 if (isset($_POST['ip'])) {
-    $my_ip = insert_ip();
-    
+
     $ip = gethostbyname(urldecode($_POST['ip']));
     if (filter_var($_POST['ip'], FILTER_VALIDATE_IP))
         $request_ip = insert_request($_POST['ip']);
     elseif (strcmp($ip, $_POST['ip']) != 0)
         $request_ip = insert_request($ip);
 } elseif (isset($_POST['domain'])) {
-    $my_ip = insert_ip();
 
     $ip = gethostbyname(urldecode($_POST['domain']));
     if (filter_var($_POST['domain'], FILTER_VALIDATE_IP))
@@ -19,7 +18,5 @@ if (isset($_POST['ip'])) {
     elseif (strcmp($ip, $_POST['domain']) != 0) {
         $request_ip = insert_request($ip);
     }
-} else {
-    $my_ip = insert_ip();
 }
 ?>
