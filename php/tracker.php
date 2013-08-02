88e434c9bf9c68762b26cf694cc8522fa5853386
Alex Hughes <alexhughes117@gmail.com>
2013-08-02 19:51:11 +0100

    Finished with whois system

--- php/tracker.php
+++ php/tracker.php
@@ -2,14 +2,13 @@
 
 require_once('src/ip.php');
 
-if (isset($_POST['ip']))
-    $my_ip = insert_ip();
+if (isset($_POST['ip'])) {
+    $ip = gethostbyname(urldecode($_POST['ip']));
 
-$ip = gethostbyname(urldecode($_POST['ip']));
-if (filter_var($_POST['ip'], FILTER_VALIDATE_IP))
-    $request_ip = insert_request($_POST['ip']);
-elseif (strcmp($ip, $_POST['ip']) != 0) {
-    $request_ip = insert_request($ip);
+    if (filter_var($_POST['ip'], FILTER_VALIDATE_IP))
+        $request_ip = insert_request($_POST['ip']);
+    elseif (strcmp($ip, $_POST['ip']) != 0)
+        $request_ip = insert_request($ip);
 } elseif (isset($_POST['domain'])) {
     $my_ip = insert_ip();
 
