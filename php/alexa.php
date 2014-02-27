8f8d84f7aae198cf816c3e7dfde0b4d60d27c778
Alex Hughes <ahughes@ahughes.org>
2014-02-27 21:56:18 +0000

    added alexa file

--- php/alexa.php
+++ php/alexa.php
@@ -9,7 +9,6 @@ function alexa_rank($url){
     $xml = simplexml_load_file("http://data.alexa.com/data?cli=10&url=".$url);
     if(isset($xml->SD)):
         return $xml->SD->REACH->attributes();
-        var_dump($xml->SD->REACH->attributes());
     endif;
 }
 
